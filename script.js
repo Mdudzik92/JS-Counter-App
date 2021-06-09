3af6004fa615daddff45786c9a9171d803e30c80
Matt Dudzik <mdudzik92@gmail.com>
2021-06-09 15:24:15 -0500

    Setting up JS variables to communicate to HTML

---
+++ script.js
@@ -0,0 +1,7 @@
+// Setting the initial state of count to 0 and putting it in its own variable.
+var count = 0;
+
+// Wiring our JS to the HTML file.
+var incrementEl = document.querySelector("#increment");
+var decrementEl = document.querySelector("#decrement");
+var countEl = document.querySelector("#count");
